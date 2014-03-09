36bb77f89553eb4ed3726ba5de4413ede65ce800
Brandon White <brandontwhite@brandon-whites-macbook-pro.whitehouse>
2014-03-08 20:46:17 -0600

    added a new class

---
+++ CC.h
@@ -0,0 +1,13 @@
+//
+//  CC.h
+//  Git Test
+//
+//  Created by Brandon White on 3/8/14.
+//  Copyright (c) 2014 Brandon White. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CC : NSObject
+
+@end
---
+++ CC.m
@@ -0,0 +1,13 @@
+//
+//  CC.m
+//  Git Test
+//
+//  Created by Brandon White on 3/8/14.
+//  Copyright (c) 2014 Brandon White. All rights reserved.
+//
+
+#import "CC.h"
+
+@implementation CC
+
+@end
