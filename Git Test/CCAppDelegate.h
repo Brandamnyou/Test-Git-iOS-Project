92baf3ff6f86bfdf248feb84a491b9190edaf155
Brandon White <brandontwhite@brandon-whites-macbook-pro.whitehouse>
2014-03-08 20:40:04 -0600

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Brandon White on 3/8/14.
+//  Copyright (c) 2014 Brandon White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Brandon White on 3/8/14.
+//  Copyright (c) 2014 Brandon White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
